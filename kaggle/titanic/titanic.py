2debf86487610bc19f0536f8e27188bce74a1af5
yungbyun <yungcheolbyun@gmail.com>
2019-01-14 19:13:56 +0900

    adf

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-np.core
 
 # 데이터 로드하기
 train = pd.read_csv('input/train.csv')
@@ -9,7 +7,7 @@ test = pd.read_csv('input/test.csv')
 # 데이터 정보
 #print(train.shape)  # 891, 12
 #print(test.shape)  # 418, 11
-print(train.head(5))
+#print(train.head(5))
 #print(test.head(5))
 
 # 데이터 체크
@@ -18,14 +16,29 @@ print(train.head(5))
 
 #print(train.isnull().sum())
 
-import matplotlib.pyplot as plt
 
-# 성별 생존자와 사망자 표시하기
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn-dark')
+sns.set(font_scale=1.5)
+
+import missingno as msno
+import warnings
+warnings.filterwarnings('ignore')
+
+#print(train['Survived'])
+#print(train['Survived'] == 1)
+#print(train[train['Survived'] == 1])
+#print(train[train['Embarked'] == 'C'])
+
+# 생존자의 남여 수, 사망자의 남여 수 표시
 survived = train[train['Survived'] == 1]['Sex'].value_counts()
 dead = train[train['Survived'] == 0]['Sex'].value_counts()
 df = pd.DataFrame([survived, dead])
 df.index = ['Survived', 'Dead']
-df.plot(kind='bar', stacked=True, figsize=(10, 5))
-plt.show()
+df.plot(kind='bar', stacked=True, figsize=(15, 10))
+#plt.show()
 
 
